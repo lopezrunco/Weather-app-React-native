cb81bd1e659f46a4bb03cd5bb785d0d6a2461aa9
lopezrunco <lopezrunco@gmail.com>
2021-10-06 17:51:54 -0300

    API connection

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const API_KEY = "097acbb8da532953b9e059038486c75e"
 
 export default function App() {
+
+  const [weatherData, setWeatherData] = useState(null)
+  const [loaded, setLoaded] = useState(true)
+
+  {/* Obtiene los datos del clima usando el nombre de la ciudad como parametro */}
+  async function fetchWeatherData(cityName) {
+    setLoaded(false)
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
+
+    /* Se conecta a la API. Si la respuesta esta OK, obtiene los datos y los setea en setWeatherData */
+    try {
+      const response = await fetch(API)
+      if(response.status == 200) {
+        const data = await response.json()
+        setWeatherData(data)
+      } else {
+        setWeatherData(null)
+      }
+      setLoaded(true)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchWeatherData('Mumbai')
+    console.log(weatherData)
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +49,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
